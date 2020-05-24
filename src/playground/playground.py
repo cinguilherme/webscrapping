e1321f24a52404c93043c32c76b9902e8802c788
Guilherme Cintra (gcintra2) <gcintra2@na.xom.com>
2020-05-24 12:56:04 -0300

    upgrades to the image file to run chromium and puppetter

--- src/playground/playground.py
+++ src/playground/playground.py
@@ -2,11 +2,34 @@ import bs4
 import time
 import schedule
 
+import asyncio
+from pyppeteer import launch
+
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
 print("playgorund ran successfully")
 print("this will keep scraping on a time based")
+
+
+async def scrapeWithPup():
+    browser = await launch(options={'args': ['--no-sandbox']})
+    page = await browser.newPage()
+    await page.goto('http://www.google.com')
+    dimensions = await page.evaluate('''() => {
+        return {
+            width: document.documentElement.clientWidth,
+            height: document.documentElement.clientHeight,
+            deviceScaleFactor: window.devicePixelRatio,
+        }
+    }''')
+
+    print(dimensions)
+    # >>> {'width': 800, 'height': 600, 'deviceScaleFactor': 1}
+    await browser.close()
+
+
+asyncio.get_event_loop().run_until_complete(scrapeWithPup())
 
 
 def scrape():
@@ -19,8 +42,8 @@ def scrape():
     print(page_soup)
 
 
-schedule.every(10).seconds.do(scrape)
+# schedule.every(10).seconds.do(scrape)
 
-while True:
-    schedule.run_pending()
-    time.sleep(9)
+# while True:
+#     schedule.run_pending()
+#     time.sleep(5)
